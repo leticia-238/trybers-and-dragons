01231ec782affc08d7d3acb344a56c89abb67721
Letícia Oliveira Andrade <leticiaoliveiraandrade.9@gmail.com>
2022-08-09 22:56:27 +0000

    feat: criação da classe Halfling como herança da classe Race

---
+++ src/Races/Halfling.ts
@@ -0,0 +1,22 @@
+import Race from './Race';
+
+class Halfling extends Race {
+  constructor(
+    name: string, 
+    dexterity: number,
+    private _maxLifePoints: number = 60,
+  ) {
+    super(name, dexterity);
+    Halfling._createdRacesInstances += 1;
+  }
+  
+  get maxLifePoints(): number {
+    return this._maxLifePoints;
+  } 
+  
+  static createdRacesInstances(): number {
+    return Halfling._createdRacesInstances;
+  }
+}
+
+export default Halfling;
