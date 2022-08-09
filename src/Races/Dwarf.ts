3649b81328757f5b9a936e40a8678600748529e4
Letícia Oliveira Andrade <leticiaoliveiraandrade.9@gmail.com>
2022-08-09 22:55:50 +0000

    feat: criação da classe Dwarf como herança da classe Race

---
+++ src/Races/Dwarf.ts
@@ -0,0 +1,22 @@
+import Race from './Race';
+
+class Dwarf extends Race {
+  constructor(
+    name: string, 
+    dexterity: number,
+    private _maxLifePoints: number = 80,
+  ) {
+    super(name, dexterity);
+    Dwarf._createdRacesInstances += 1;
+  }
+  
+  get maxLifePoints(): number {
+    return this._maxLifePoints;
+  } 
+  
+  static createdRacesInstances(): number {
+    return Dwarf._createdRacesInstances;
+  }
+}
+
+export default Dwarf;
