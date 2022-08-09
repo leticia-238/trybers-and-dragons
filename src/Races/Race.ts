19e01dcc8715fd1a26df8bf5be1822c199574699
Letícia Oliveira Andrade <leticiaoliveiraandrade.9@gmail.com>
2022-08-09 23:46:10 +0000

    style(Race): retira identação e quebra de linha de função

--- src/Races/Race.ts
+++ src/Races/Race.ts
@@ -6,13 +6,8 @@ abstract class Race {
     private _dexterity: number,
   ) {}
   
-  get name(): string {
-    return this._name;
-  }
-  
-  get dexterity(): number {
-    return this._dexterity;
-  }
+  get name(): string { return this._name; }
+  get dexterity(): number { return this._dexterity; }
   
   abstract get maxLifePoints(): number; 
   
